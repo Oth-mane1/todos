a1ae45e194890458b2a053692257e5ef1ec025c2
Othmane Ouirdy <55802592+Oth-mane1@users.noreply.github.com>
2022-05-09 00:14:20 +0100

    Optimize the code base and add more functionalities

--- js/todo.js
+++ js/todo.js
@@ -1,107 +1,121 @@
+const LS_DATE = "date";
+const LS_TODO = "todos";
+window.TASKID = 0;
+
 var date = new Date();
-var itemsTd = JSON.parse(localStorage.getItem("todos"));
+var notyf = new Notyf({
+    duration: 3500,
+    dismissible: true,
+    ripple: true,
+    position: {
+        x: 'center',
+        y: 'top',
+    }
+});
 
-// Check if the local storage is empty
-if (localStorage.length == 0) {
+function initLocalStorage() {
     // So initialize it with an empty todo and the current day
-    localStorage.setItem("date", date.getDate().toString());
-    localStorage.setItem("todos", JSON.stringify(""));
-    itemsTd = JSON.parse(localStorage.getItem("todos"));
+    localStorage.setItem(LS_DATE, date.getDate().toString());
+    localStorage.setItem(LS_TODO, JSON.stringify([]));
 }
 
-// Check if we are passed the past day
-if (localStorage.getItem("date") != date.getDate()) {
-    // Then clear it
-    localStorage.setItem("todos", JSON.stringify(""));
-    localStorage.setItem("date", date.getDate().toString());
+// Get item from the local storage
+function getLsItem(item) {
+    var lsItem = JSON.parse(localStorage.getItem(item));
+    return lsItem ? lsItem : []
 }
 
-// Add the existing elemts to the todos
-for (var i = 0; i < itemsTd.length; i++) {
-    console.log(itemsTd);
+// Set item in the local storage
+function setLsItem(item, value) {
+    return localStorage.setItem(item, JSON.stringify(value));
+}
+
+// Create a new task in the DOM and in the local storage 
+function createNewTask(tskText, tskChecked = false, isNewTask = true) {
+    ++window.TASKID
+    // Create the parent li 
     var li = document.createElement("li");
-    var inputValue = itemsTd[i];
-    var t = document.createTextNode(inputValue);
+    var t = document.createTextNode(tskText);
     li.appendChild(t);
-    document.getElementById("TdList").appendChild(li);
 
+    // Create the close span
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
+    // Append the click event to hide the checked task
+    span.addEventListener("click", function (e) {
+        var prntLi = this.parentElement;
+        var items = getLsItem(LS_TODO).filter(o => o && o.id != prntLi.getAttribute("data-id"));
+        setLsItem(LS_TODO, items);
+        prntLi.remove();
+    })
     li.appendChild(span);
-}
-
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.display = "none";
+    li.setAttribute("data-id", window.TASKID);
+    if (tskChecked) {
+        li.classList.add("checked");
     }
+
+    document.getElementById("TdList").appendChild(li);
+
+    if (!isNewTask) {
+        return
+    }
+
+    var itemsTd = getLsItem(LS_TODO);
+    itemsTd.push({
+        "id": window.TASKID,
+        "checked": tskChecked,
+        "text": tskText,
+    });
+    setLsItem(LS_TODO, itemsTd);
 }
 
+// Check if the local storage is empty
+// Check if we are passed the past day
+if (localStorage.length == 0 || localStorage.getItem(LS_DATE) != date.getDate()) {
+    initLocalStorage()
+}
 
-var addbtn = document.getElementById("add");
+var savedItems = JSON.parse(localStorage.getItem(LS_TODO));
+savedItems = !savedItems ? [] : savedItems;
 
-document.addEventListener("load", function () {
-
+window.addEventListener("load", function () {
+    // Add the existing elemts to the todos
+    var itemsTd = getLsItem(LS_TODO);
+    itemsTd.forEach(item => {
+        item && createNewTask(item.text, item.checked, false);
+    });
 });
 
 // Create a new list item and Add it to the local storage
+var addbtn = document.getElementById("add");
 addbtn.addEventListener("click", function () {
+    notyf.dismissAll();
     var titre = document.getElementById("titre");
     if (!titre.value) {
-        alert("Veuillez entrer un titre");
+        notyf.error('Please fill up the title!');
+        titre.classList.add("missed-value");
     } else {
-        var li = document.createElement("li");
-        var inputValue = document.getElementById("titre").value;
-        var t = document.createTextNode(inputValue);
-        li.appendChild(t);
-        document.getElementById("TdList").appendChild(li);
-
-        var span = document.createElement("SPAN");
-        var txt = document.createTextNode("\u00D7");
-        span.className = "close";
-        span.appendChild(txt);
-        li.appendChild(span);
-
-        for (i = 0; i < close.length; i++) {
-            close[i].onclick = function () {
-                var div = this.parentElement;
-                div.style.display = "none";
-            }
-        }
-
-        var itemsTd = JSON.parse(localStorage.getItem("todos"));
-        itemsTd[itemsTd.length] = titre.value;
-        localStorage.setItem("todos", JSON.stringify(itemsTd));
+        var inputValue = titre.value.trim();
+        createNewTask(inputValue);
         titre.value = "";
+        titre.classList.remove("missed-value");
+        notyf.success('Task added successfully!');
     }
 });
-
-// Create a "close" button and append it to each list item
-var myNodelist = document.querySelectorAll("#TdList li");
-for (var i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-for (var i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.a
-        div.style.display = "none";
-    }
-}
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('.todo-box ul');
 list.addEventListener('click', function (ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
+        var items = getLsItem(LS_TODO);
+        items.map(o => {
+            if (o && o.id == ev.target.getAttribute("data-id")) {
+                return o.checked = ev.target.classList.contains("checked")
+            }
+        });
+        setLsItem(LS_TODO, items);
     }
 }, false);
